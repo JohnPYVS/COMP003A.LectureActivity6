5a0a48c2789e73d34ac1cb5e63028ead9543a989
JohnVisual <johnpatrickdeguzman149@gmail.com>
2026-02-12 22:30:42 -0800

    All of the steps are completed

--- COMP003A.LectureActivity6/Program.cs
+++ COMP003A.LectureActivity6/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student1 = new Student("Alex", 20);
+            Student student2 = new Student("Jordan", 22);
+
+            student1.Displayinfo();
+            student2.Displayinfo();
+
+            student1.Age = 21;
+
+            student1.Displayinfo();
+            student2.Displayinfo();
+
+            /* Reflection
+             * 
+             * Each student object have its own data because each of the student has it's own stored fields.
+             * 
+             * Main does not need to know how Displayinfo works because it only needs to know that it exist, and what it does.
+             * 
+             * This activity demonstrate encapsulation conceptually because Main just uses the class thorugh its methods
+             * and both fields and the Displayinfo just helps the class on how the line is formatted.
+             */
+
         }
     }
 }
---
+++ COMP003A.LectureActivity6/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.LectureActivity6
+{
+    internal class Student
+    {
+        public string Name;
+        public int Age;
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Displayinfo()
+        {
+            Console.WriteLine("Name: " + Name + ", Age: " + Age);
+        }
+    }
+}
